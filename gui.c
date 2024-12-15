b43070181a23cfdbf509fab6a3dffb1d3e04372b
minseon <iu4001@naver.com>
2024-12-16 05:39:06 +0900

    firts

--- gui.c
+++ gui.c
@@ -15,6 +15,8 @@
 GtkTextBuffer *text_buffer;
 GtkEntry *entry;
 
+void send_file_to_server(const char *file_path);
+
 void *receive_messages_gui(void *arg) {
     char buffer[BUFFER_SIZE];
     int bytes_read;
@@ -59,18 +61,28 @@ void *receive_messages_gui(void *arg) {
     return NULL;
 }
 
-void send_message(GtkWidget *widget, gpointer data) {
-    const char *message = gtk_entry_get_text(entry);
-    if (strlen(message) > 0) {
-        send_message_to_server(message);
 
-        char formatted_message[BUFFER_SIZE];
-        snprintf(formatted_message, sizeof(formatted_message), "Me > %s\n", message);
-        gtk_text_buffer_insert_at_cursor(text_buffer, formatted_message, -1);
 
-        gtk_entry_set_text(entry, "");
+
+/*
+// 서버에 파일을 전송하는 함수
+void send_file(GtkWidget *widget, gpointer data) {
+    GtkFileChooser *chooser = GTK_FILE_CHOOSER(gtk_file_chooser_dialog_new(
+        "Choose a file", NULL, GTK_FILE_CHOOSER_ACTION_OPEN,
+        "Cancel", GTK_RESPONSE_CANCEL,
+        "Open", GTK_RESPONSE_ACCEPT,
+        NULL
+    ));
+
+    if (gtk_dialog_run(GTK_DIALOG(chooser)) == GTK_RESPONSE_ACCEPT) {
+        char *file_name = gtk_file_chooser_get_filename(chooser);
+        send_file_to_server(sock, file_name);
+        g_free(file_name);
     }
+
+    gtk_widget_destroy(GTK_WIDGET(chooser));
 }
+*/
 
 // 파일 전송 함수
 void send_file_to_server(const char *file_path) {
@@ -96,22 +108,44 @@ void send_file_to_server(const char *file_path) {
     gtk_text_buffer_insert_at_cursor(text_buffer, "File sent successfully.\n", -1);
 }
 
+
 // 파일 선택 및 업로드 함수
 void on_file_upload_button_clicked(GtkWidget *widget, gpointer data) {
-    GtkWidget *dialog;
-    dialog = gtk_file_chooser_dialog_new("Open File", NULL, GTK_FILE_CHOOSER_ACTION_OPEN,
-                                        "Cancel", GTK_RESPONSE_CANCEL,
-                                        "Open", GTK_RESPONSE_ACCEPT,
-                                        NULL);
+    GtkWidget *dialog = gtk_file_chooser_dialog_new(
+        "Choose a file", NULL, GTK_FILE_CHOOSER_ACTION_OPEN,
+        "Cancel", GTK_RESPONSE_CANCEL,
+        "Open", GTK_RESPONSE_ACCEPT,
+        NULL
+    );
 
     if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
         char *file_path = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
-        send_file_to_server(file_path);  // 파일 전송
+
+        // 파일을 서버에 전송
+        send_file_to_server(file_path);
+
         g_free(file_path);
     }
 
     gtk_widget_destroy(dialog);
 }
+
+
+
+void send_message(GtkWidget *widget, gpointer data) {
+    const char *message = gtk_entry_get_text(entry);
+    if (strlen(message) > 0) {
+        send_message_to_server(message);
+
+        char formatted_message[BUFFER_SIZE];
+        snprintf(formatted_message, sizeof(formatted_message), "Me > %s\n", message);
+        gtk_text_buffer_insert_at_cursor(text_buffer, formatted_message, -1);
+
+        gtk_entry_set_text(entry, "");
+    }
+}
+
+
 
 int main(int argc, char *argv[]) {
     pthread_t recv_thread;
